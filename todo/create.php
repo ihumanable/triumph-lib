8b98a9afc369807c87e4b42cd49d50e01f9340e8
Matt Nowack <mdnowack@gmail.com>
2009-11-25 09:39:52 -0500

    fixing some tab issues

--- todo/create.php
+++ todo/create.php
@@ -1,19 +1,18 @@
 <?php
-	require_once 'config.php';
-	
-	if($_SERVER['REQUEST_METHOD'] == 'POST') {
-		$todo = new Todo();
+  require_once 'config.php';
+
+  if($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $todo = new Todo();
     $todo->title = $_POST['title'];
     $todo->save();
     
-		header("Location: index.php");
-	} else {
-		include_once 'header.php';
-
+    header("Location: index.php");
+  } else {
+    include_once 'header.php';
+    
     display_form("create.php", "create");
-
-		include_once 'footer.php';
-	}
-
+  
+    include_once 'footer.php';
+  }
 
 ?>
--- todo/delete.php
+++ todo/delete.php
@@ -1,21 +1,19 @@
 <?php
-	require_once 'config.php';
-	
-	
-	if($_SERVER['REQUEST_METHOD'] == 'POST') {
-		//Delete the todo and redirect
-		$todo = new Todo($_POST['id']);
-		$todo->delete();
-		
-    header("Location: index.php");
-	} else {
-		include_once 'header.php';
-		
-    $todo = new Todo($_GET['id']);
-		display_form("delete.php", "delete", "Delete {$todo->title}?", false);
-		
+  require_once 'config.php';
 
-		include_once 'footer.php';
-	}
-		
+  if($_SERVER['REQUEST_METHOD'] == 'POST') {
+    //Delete the todo and redirect
+    $todo = new Todo($_POST['id']);
+    $todo->delete();
+    
+    header("Location: index.php");
+  } else {
+    include_once 'header.php';
+    
+    $todo = new Todo($_GET['id']);
+    display_form("delete.php", "delete", "Delete {$todo->title}?", false);
+    
+    include_once 'footer.php';
+  }
+
 ?>
